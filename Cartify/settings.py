c35072c52a46909c463ad4fe1e3669b9bea281af
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-01-28 16:41:14 +0500

    added context processor for cart count

--- Cartify/settings.py
+++ Cartify/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 "users.context_processors.wishlist_count",
+                "users.context_processors.cart_count",
             ],
         },
     },
--- users/context_processors.py
+++ users/context_processors.py
@@ -1,4 +1,5 @@
 from .models import WishlistItem
+from cart.models import CartItem
 
 def wishlist_count(request):
     if request.user.is_authenticated:
@@ -6,3 +7,12 @@ def wishlist_count(request):
     else:
         count = 0
     return {"wishlist_count": count}
+
+def cart_count(request):
+
+    if request.user.is_authenticated:
+        count = CartItem.objects.filter(user=request.user).count()
+    else: 
+        count = 0
+        
+    return {"cart_count": count}
