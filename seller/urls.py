92206b292de629eea2e231aa047b5d86cdece100
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-02-03 14:53:09 +0500

    specified URLs

--- seller/urls.py
+++ seller/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from seller.views import *
+from seller.views import dashboard, products
 
 urlpatterns = [
-    path('', dashboard, name='dashboard')
+    path("", dashboard, name="dashboard"),
+    path("products/", products, name="seller_products"),
 ]
