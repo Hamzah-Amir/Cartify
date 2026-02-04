a6b1148d5700a80934df976cfaeb2416f1b6fe30
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-02-04 21:06:16 +0500

    views for adding product

--- seller/urls.py
+++ seller/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from seller.views import dashboard, inventory, orders, manage_order
+from seller.views import dashboard, inventory, orders, manage_order, add_listing
 
 urlpatterns = [
     path("", dashboard, name="dashboard"),
     path("inventory/", inventory, name="inventory"),
     path("orders/", orders, name="orders"),
     path("orders/<int:order_id>/detail/", manage_order, name="manage_order"),
+    path("products/add_listing/", add_listing, name="add_listing"),
 ]
--- seller/views.py
+++ seller/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from products.models import Product
+from products.models import Product, ProductImage
 from cart.models import Order
 
 
@@ -59,3 +59,34 @@ def manage_order(request, order_id):
         }
         print(context)
         return render(request, "seller/order_detail.html", context)
+
+def add_listing(request):
+    if request.user.is_anonymous:
+        return redirect('loginUser')
+    if request.user.role != 'seller':
+        return redirect('home')
+    if request.method == "GET" and request.user.is_authenticated and request.user.role == 'seller':
+        return render(request, "seller/add_listing.html")
+    if request.method == "POST" and request.user.is_authenticated and request.user.role == 'seller':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        category = request.POST.get('category')
+        price = request.POST.get('price')
+        stock = request.POST.get('stock')
+        image = request.FILES.get('image')
+        additional_images = request.FILES.getlist('additional_images')
+        product = Product.objects.create(
+            name=name,
+            description=description,
+            category=category,
+            price=price,
+            stock=stock,
+            image=image,
+            seller=request.user,
+        )
+        for additional_image in additional_images:
+            ProductImage.objects.create(
+                product=product,
+                image=additional_image,
+            )
+        return redirect('dashboard')
