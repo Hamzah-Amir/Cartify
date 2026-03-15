1726608b2897e506dc87f5824f4b1877e8cbbdfa
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-03-15 16:48:50 +0500

    Changed database password

--- Cartify/settings.py
+++ Cartify/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cartify',
         'USER': 'postgres',
-        'PASSWORD': "Hamza",
+        'PASSWORD': "H@mza",
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
