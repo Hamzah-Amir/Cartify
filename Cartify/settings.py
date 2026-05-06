c4dac8dd2d645374d8fcdb52cdeb60bd59f75a2f
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-05-06 15:26:39 +0500

    updated DB URL

--- Cartify/settings.py
+++ Cartify/settings.py
@@ -80,15 +80,12 @@ WSGI_APPLICATION = 'Cartify.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cartify',
-        'USER': 'postgres',
-        'PASSWORD': "H@mza",
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
