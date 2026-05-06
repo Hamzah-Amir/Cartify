a42a18609d170c734a12d43029e40839bd0cf533
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-05-06 15:56:59 +0500

    updated requirement.txt and db url

--- Cartify/settings.py
+++ Cartify/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-lj*_44&u$hd(4(l7bt36l$0xw8i_2134h1dm021p)!2%%uozcx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
