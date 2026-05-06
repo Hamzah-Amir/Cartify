b2f0f5994dc2e8e25653605fad77bec1bd0ecdbc
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-05-06 15:45:19 +0500

    updated database url

--- Cartify/settings.py
+++ Cartify/settings.py
@@ -83,9 +83,7 @@ WSGI_APPLICATION = 'Cartify.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-       dj_database_url.config(default=os.getenv('DATABASE_URL'))
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 AUTH_USER_MODEL = 'users.CustomUser'
